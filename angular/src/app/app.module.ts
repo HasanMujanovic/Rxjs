723127470e874902cc0620b374bc1a498babddcc
HasanMujanovic <respiratori1234@gmail.com>
2024-05-01 20:36:43 +0200

    edit

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,17 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TrainComponent } from './train/train.component';
 import { ExerciseObsSubComponent } from './exercise-obs-sub/exercise-obs-sub.component';
+import { FromComponent } from './creation-operators/from/from.component';
+import { OfComponent } from './creation-operators/of/of.component';
+import { TimerComponent } from './creation-operators/timer/timer.component';
+import { IntervalComponent } from './creation-operators/interval/interval.component';
+import { ForkJoinComponent } from './creation-operators/fork-join/fork-join.component';
+import { CombineLatestComponent } from './creation-operators/combine-latest/combine-latest.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TrainComponent,
-    ExerciseObsSubComponent
+    ExerciseObsSubComponent,
+    FromComponent,
+    OfComponent,
+    TimerComponent,
+    IntervalComponent,
+    ForkJoinComponent,
+    CombineLatestComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

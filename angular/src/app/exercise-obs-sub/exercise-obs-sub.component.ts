306678786e040d825724d7e18a4ebfd7c82841f0
HasanMujanovic <respiratori1234@gmail.com>
2024-05-01 15:42:47 +0200

    cancellation-unsubscribe

--- angular/src/app/exercise-obs-sub/exercise-obs-sub.component.ts
+++ angular/src/app/exercise-obs-sub/exercise-obs-sub.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, count } from 'rxjs';
 
 @Component({
   selector: 'app-exercise-obs-sub',
@@ -7,30 +7,51 @@ import { Observable } from 'rxjs';
   styleUrls: ['./exercise-obs-sub.component.css'],
 })
 export class ExerciseObsSubComponent {
-  observable$ = new Observable<any>((subscriber) => {
-    console.log('Observable executed');
-    subscriber.next('value1');
-    subscriber.next('value2');
-    setTimeout(() => {
-      subscriber.error(new Error('Failure'));
+  // observable$ = new Observable<any>((subscriber) => {
+  //   console.log('Observable executed');
+  //   subscriber.next('value1');
+  //   subscriber.next('value2');
+  //   setTimeout(() => {
+  //     subscriber.error(new Error('Failure'));
+  //   }, 2000);
+  //   setTimeout(() => {
+  //     subscriber.next('value3');
+  //   }, 4000);
+
+  //   return () => {
+  //     console.log('Teardown');
+  //     subscriber.complete();
+  //   };
+  // });
+
+  // constructor() {
+  //   console.log('Before subscribe');
+  //   this.observable$.subscribe({
+  //     next: (value) => console.log(value),
+  //     error: (err) => console.log(err.message),
+  //     complete: () => console.log('Completed'),
+  //   });
+  //   console.log('After subscription');
+  // }
+
+  observable2$ = new Observable<number>((subscriber) => {
+    let counter = 1;
+    const intervalId = setInterval(() => {
+      console.log('Emitted', counter);
+      subscriber.next(counter++);
     }, 2000);
-    setTimeout(() => {
-      subscriber.next('value3');
-    }, 4000);
 
     return () => {
-      console.log('Teardown');
-      subscriber.complete();
+      clearInterval(intervalId);
     };
   });
-
+  subscription = this.observable2$.subscribe((value) => console.log(value));
   constructor() {
-    console.log('Before subscribe');
-    this.observable$.subscribe({
-      next: (value) => console.log(value),
-      error: (err) => console.log(err.message),
-      complete: () => console.log('Completed'),
-    });
-    console.log('After subscription');
+    console.log('Start subscription');
+    this.subscription;
+    setTimeout(() => {
+      console.log('unsubscribe');
+      this.subscription.unsubscribe();
+    }, 7000);
   }
 }
